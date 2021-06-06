1157db576f1dbb3325f44b8fc1e359b2611083f8
levi749 <84772173+levi749@users.noreply.github.com>
2021-06-06 19:58:10 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -7,9 +7,9 @@ class Translation(object):
     START_TEXT = """<b>Hey {}!!</b>
 <i>Am Just A Advance Auto Filter Bot....😉
 
-Just Add Me To Your Group And Channel And Connect Them And See My Pevers 🔥🔥😝
 
-For More Details Click Help Button Below..
+
+For More Details Join group
 @CrazyBotsz
 </i>"""    
     
@@ -43,12 +43,12 @@ For More Details Click Help Button Below..
             
             -> <code>Status</code> - Button Will Shows The Stats Of Your Channel
             
-@CrazyBotsz
+@Cl_chatgrp
 """
     
     ABOUT_TEXT = """<b>➥ Name</b> : <code> Auto Filter Bot</code>
     
-<b>➥ Creator</b> : <b><i><a href="https://t.me/AlbertEinstein_TG">AlbertEinstein_TG</a></i></b>
+<b>➥ Creator</b> : <b><i><a href="https://t.me/Levinomad_TG">Levi</a></i></b>
 
 <b>➥ Language</b> : <code>Python3</code>
 
